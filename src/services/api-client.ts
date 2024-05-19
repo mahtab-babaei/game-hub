a5d0cc92b21de6f284a565871f97e71a5f5bed16
Mahtab <mahtaab.babaei@gmail.com>
2024-05-19 11:00:41 +0430

    Fetch game details

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -18,13 +18,19 @@ class APIClient<T> {
 
   constructor(endpoint: string) {
     this.endpoint = endpoint;
-  }
+  };
 
   getAll = (config: AxiosRequestConfig) => {
     return axiosInstance
       .get<FetchResponse<T>>(this.endpoint, config)
       .then(res => res.data)
-  }
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + '/' + id)
+      .then(res => res.data)
+  }; 
 }
 
 export default APIClient;
