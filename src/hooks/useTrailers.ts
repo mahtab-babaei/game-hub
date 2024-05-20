3c83eeb7dc0014971c169f6e4a334973e53828f1
Mahtab <mahtaab.babaei@gmail.com>
2024-05-20 14:59:28 +0430

    Fix an issue of trailers endpoint

--- src/hooks/useTrailers.ts
+++ src/hooks/useTrailers.ts
@@ -6,7 +6,7 @@ const useTrailers = (gameId: number) => {
   const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
 
   return useQuery({
-    queryKey: ['games', gameId],
+    queryKey: ['trailers', gameId],
     queryFn: apiClient.getAll
   })
 }
