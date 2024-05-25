d41443076860907b372127bb243439e7877a9601
Mahtab <mahtaab.babaei@gmail.com>
2024-05-25 14:20:52 +0430

    Fix the issue of logo in small devices

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,14 +1,21 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, Show, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <HStack padding="10px">
       <Link to="/">
-        <Image src={logo} boxSize="60px" objectFit="cover" />
+        <Show breakpoint="(min-width: 676px)">
+          <Image src={logo} boxSize="60px" objectFit="cover" />
+        </Show>
+        <Show breakpoint="(max-width: 675px)">
+          <Text fontWeight="bold" whiteSpace="nowrap">
+            R A W G
+          </Text>
+        </Show>
       </Link>
       <SearchInput />
       <ColorModeSwitch />
